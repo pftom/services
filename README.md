213d26e3163066d9704ca0530f73da2d520f3089
黄巍 <huan1043269994@gmail.com>
2017-11-17 16:09:15 +0800

    [ADD] add some modify.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 git clone git@github.com:pftom/services.git
 
 cd services && npm install
+
 node index.js
 ```
--- index.js
+++ index.js
@@ -3,19 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // notification api
-// When need update to next question, GET /push_notification
+// When need to be updated to the next question, GET /push_notification
 app.get('/push_notification', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  // response note
+  res.send('Successfully responded');
   // create connection
+  // After frontend create socket.io instance, this can be bi-direction communication
   io.on('connection', function (socket) {
     // print a user is connect
     console.log('a user connected');
     // listen on `push notification` event and notify client for response
-      // 
     io.emit('push notification', 'start response for server');
   });
 });
 
+// This server is listening on 3000 port
 http.listen(3000, function () {
   console.log('listening on *.3000');
 });
