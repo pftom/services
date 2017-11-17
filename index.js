7236f5cd06625d637cf43a0144587044103214b6
黄巍 <huan1043269994@gmail.com>
2017-11-17 17:13:15 +0800

    [ADD] add socket connection on global.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var io = require('socket.io')(http);
 
 // notification api
 // When need to be updated to the next question, GET /push_notification
-app.get('/push_notification', function (req, res) {
-  // response note
-  res.send('Successfully responded');
-  // create connection
-  // After frontend create socket.io instance, this can be bi-direction communication
-  io.on('connection', function (socket) {
+// create connection
+ // After frontend create socket.io instance, this can be bi-direction communication
+io.on('connection', function (socket) {
+  app.get('/push_notification', function (req, res) {
+    // response note
+    res.send('Successfully responded');
     // print a user is connect
     console.log('a user connected');
     // listen on `push notification` event and notify client for response
@@ -18,6 +18,6 @@ app.get('/push_notification', function (req, res) {
 });
 
 // This server is listening on 3000 port
-http.listen(3000, function () {
+http.listen(8000, function () {
   console.log('listening on *.3000');
 });
