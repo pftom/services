52c7593fda4e4e43fad64cd2d18fcdbebff2203e
mRcfps <1402491442@qq.com>
2017-11-17 16:54:22 +0800

    [ADD] fabric automation script

---
+++ fabfile.py
@@ -0,0 +1,38 @@
+from fabric.api import *
+
+env.hosts = ['root@60.205.183.134']
+
+image_repo = 'registry.cn-hangzhou.aliyuncs.com/csthome/node'
+container_name = 'csthome_node'
+
+
+def build_and_push_image():
+    """Build docker image from the code and push to registry."""
+    local("docker build -t %s ." % image_repo)
+    local("docker push %s" % image_repo)
+
+
+def pull_image_and_redeploy():
+    """Pull the newest image from Docker Hub."""
+    # Pull the newest image
+    run("docker pull %s" % image_repo)
+
+    running_containers = run("docker ps --format {{.Names}}").split()
+    if container_name in running_containers:
+        # The image already has a running container
+        # So we need to remove it
+        run("docker rm -f %s" % container_name)
+
+    # Run a container with the updated image and restart proxy
+    run("docker run -d -p 3000:3000 --name %s %s" % (container_name, image_repo))
+
+
+@task
+def deploy_with_private_key():
+    build_and_push_image()
+    pull_image_and_redeploy()
+
+
+@task
+def deploy():
+    local("fab deploy_with_private_key -i pf.pem")
