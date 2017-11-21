ad146721efc64bc11a98c8eb09239eb484b0b082
黄巍 <huan1043269994@gmail.com>
2017-11-21 23:01:11 +0800

    [ADD] add cors and add update methods.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var request = require('http');
+var cors = require('cors');
 
 var {
   singleOptions,
@@ -11,7 +12,10 @@ var {
 var singleOptionIndex = 0;
 var multiplyOptionIndex = 0;
 
-var allContestants = null;
+// original data
+var allContestants = [];
+// every contest update data
+var varAllContestants = [];
 
 // when start node get the result from the Django
 var options = {
@@ -21,10 +25,15 @@ var options = {
   method: 'GET',
 };
 
+
 var req = request.request(options, function (res) {
   res.setEncoding('utf8');
   res.on('data', function (chunk) {
-    allContestants = chunk;
+    allContestants = JSON.parse(chunk);
+    
+    varAllContestants = allContestants = allContestants.map(item => (
+      Object.assign({}, item, { out: false })
+    ));
     console.log('allContestants', allContestants);
   });
 });
@@ -34,6 +43,8 @@ req.on('error', function (e) {
 });
 
 req.end();
+
+app.use(cors());
 
 // notification api
 // When need to be updated to the next question, GET /push_notification
@@ -59,13 +70,61 @@ io.on('connection', function (socket) {
     io.emit('push notification', { option, id });
   });
 
+  // update logged status
   app.get('/next_contest', function (req, res) {
+    varAllContestants = allContestants;
     res.send('Successfully responded');
     // print a user is connect
     console.log('a user connected');
     // listen on `push notification` event and notify client for response
     io.emit('next contest', 'start response for server');
-  })
+  });
+
+  app.get('/update_logged', function (req, res) {
+    const { user } = req.query;
+    // update the logged status
+    varAllContestants = varAllContestants.map(item => (
+      (String(item.user) === user) 
+      ? Object.assign({}, item, { logged: true })
+      : item
+    ));
+
+    console.log('update_logged', varAllContestants);
+
+    io.emit('logged', { user });
+    res.send(JSON.stringify('Successfully responsed'));
+  });
+
+  // update the out status
+  app.get('/update_out', function (req, res) {
+    const { user, players } = req.query;
+    console.log('user', user);
+
+    if (players) {
+      // get the remain audience
+      const remainAudience = varAllContestants
+                              .filter(item => !players.includes(item.user))
+                              .filter(item => !item.out)
+                              .length;
+      io.emit('score', { user, remainAudience });
+    }
+
+    varAllContestants = varAllContestants.map(item => (
+      (String(item.user) === user) 
+      ? Object.assign({}, item, { out: true })
+      : item
+    ));
+
+    console.log('update_out', varAllContestants);
+
+    res.send(JSON.stringify('Successfully responsed'));
+  });
+
+});
+
+app.get('/users/', function (req, res) {
+  console.log('users/', JSON.stringify(varAllContestants))
+  res.send(JSON.stringify(varAllContestants));
 });
 
 // This server is listening on 3000 port
